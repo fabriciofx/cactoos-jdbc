257fe4011f2d62f8a84d4406dc06120777f4cc85
Fabricio Cabral <fabriciofx@gmail.com>
2019-08-31 12:40:24 -0300

    [#11] Let's update Copyright notice to 2019

--- src/main/java/com/github/fabriciofx/cactoos/jdbc/result/package-info.java
+++ src/main/java/com/github/fabriciofx/cactoos/jdbc/result/package-info.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 Fabricio Barros Cabral
+ * Copyright (C) 2018-2019 Fabricio Barros Cabral
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
