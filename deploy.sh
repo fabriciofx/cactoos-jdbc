37aa8f9e2a355d82d13a36d481ab0cccae194a18
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-05-13 12:14:40 -0300

    Remove script to deploy; uses GH actions to do it

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export JDK_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED"
-mvn -Pqulice -Psonatype -Pcactoos-jdbc clean deploy -DskipTests=true --settings=../settings.xml
