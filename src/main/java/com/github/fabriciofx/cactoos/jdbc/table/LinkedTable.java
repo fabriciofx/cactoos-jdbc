9e7a829bd483b98b7d95baaf1d4d678c78e85b54
Fabrício Barros Cabral <fabriciofx@gmail.com>
2026-01-07 19:54:19 -0300

    refactor: LinkedTable now holds data

--- src/main/java/com/github/fabriciofx/cactoos/jdbc/table/LinkedTable.java
+++ src/main/java/com/github/fabriciofx/cactoos/jdbc/table/LinkedTable.java
@@ -10,6 +10,8 @@ import com.github.fabriciofx.cactoos.jdbc.Rows;
 import com.github.fabriciofx.cactoos.jdbc.Table;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import org.cactoos.Scalar;
+import org.cactoos.scalar.Sticky;
 import org.cactoos.text.Lowered;
 
 /**
@@ -18,45 +20,59 @@ import org.cactoos.text.Lowered;
  */
 public final class LinkedTable implements Table {
     /**
-     * ResultSet.
+     * Rows.
      */
-    private final ResultSet rset;
+    private final Scalar<Rows> records;
+
+    /**
+     * Columns.
+     */
+    private final Scalar<Columns> labels;
 
     /**
      * Ctor.
      * @param rset A {@link ResultSet}
      */
     public LinkedTable(final ResultSet rset) {
-        this.rset = rset;
+        this.records = new Sticky<>(
+            () -> {
+                final Rows rows = new LinkedRows();
+                final ResultSetMetaData meta = rset.getMetaData();
+                while (rset.next()) {
+                    final Row row = new LinkedRow();
+                    for (int col = 1; col <= meta.getColumnCount(); ++col) {
+                        row.add(
+                            new Lowered(meta.getColumnLabel(col)).asString(),
+                            rset.getObject(col)
+                        );
+                    }
+                    rows.add(row);
+                }
+                return rows;
+            }
+        );
+        this.labels = new Sticky<>(
+            () -> {
+                final ResultSetMetaData meta = rset.getMetaData();
+                final Columns columns = new LinkedColumns();
+                for (int col = 1; col <= meta.getColumnCount(); ++col) {
+                    columns.add(
+                        new Lowered(meta.getColumnName(col)).asString(),
+                        meta.getColumnType(col)
+                    );
+                }
+                return columns;
+            }
+        );
     }
 
     @Override
     public Rows rows() throws Exception {
-        final Rows rows = new LinkedRows();
-        final ResultSetMetaData meta = this.rset.getMetaData();
-        while (this.rset.next()) {
-            final Row row = new LinkedRow();
-            for (int col = 1; col <= meta.getColumnCount(); ++col) {
-                row.add(
-                    new Lowered(meta.getColumnLabel(col)).asString(),
-                    this.rset.getObject(col)
-                );
-            }
-            rows.add(row);
-        }
-        return rows;
+        return this.records.value();
     }
 
     @Override
     public Columns columns() throws Exception {
-        final ResultSetMetaData meta = this.rset.getMetaData();
-        final Columns columns = new LinkedColumns();
-        for (int col = 1; col <= meta.getColumnCount(); ++col) {
-            columns.add(
-                new Lowered(meta.getColumnName(col)).asString(),
-                meta.getColumnType(col)
-            );
-        }
-        return columns;
+        return this.labels.value();
     }
 }
