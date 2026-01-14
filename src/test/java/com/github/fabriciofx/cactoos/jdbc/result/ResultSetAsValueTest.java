5d9ae020830868345d3ec2f977eb25d73a4ba7b7
Fabrício Barros Cabral <fabriciofx@gmail.com>
2026-01-14 19:00:41 -0300

    test: Remove server from ResultSetAsValue test

--- src/test/java/com/github/fabriciofx/cactoos/jdbc/result/ResultSetAsValueTest.java
+++ src/test/java/com/github/fabriciofx/cactoos/jdbc/result/ResultSetAsValueTest.java
@@ -5,6 +5,7 @@
 package com.github.fabriciofx.cactoos.jdbc.result;
 
 import com.github.fabriciofx.cactoos.jdbc.Session;
+import com.github.fabriciofx.cactoos.jdbc.Source;
 import com.github.fabriciofx.cactoos.jdbc.param.IntParam;
 import com.github.fabriciofx.cactoos.jdbc.param.TextParam;
 import com.github.fabriciofx.cactoos.jdbc.query.NamedQuery;
@@ -14,9 +15,8 @@ import com.github.fabriciofx.cactoos.jdbc.source.NoAuth;
 import com.github.fabriciofx.cactoos.jdbc.statement.Insert;
 import com.github.fabriciofx.cactoos.jdbc.statement.Select;
 import com.github.fabriciofx.cactoos.jdbc.statement.Update;
-import com.github.fabriciofx.fake.server.Server;
-import com.github.fabriciofx.fake.server.db.server.H2Server;
-import javax.sql.DataSource;
+import com.github.fabriciofx.fake.server.RandomName;
+import com.github.fabriciofx.fake.server.db.source.H2Source;
 import org.junit.jupiter.api.Test;
 import org.llorllale.cactoos.matchers.Assertion;
 import org.llorllale.cactoos.matchers.HasValue;
@@ -30,40 +30,42 @@ import org.llorllale.cactoos.matchers.HasValue;
 final class ResultSetAsValueTest {
     @Test
     void value() throws Exception {
-        try (Server<DataSource> server = new H2Server()) {
-            server.start();
-            try (Session session = new NoAuth(server.resource()).session()) {
-                new Update(
-                    session,
-                    new QueryOf(
-                        """
-                        CREATE TABLE contact (id INT AUTO_INCREMENT, name
-                        VARCHAR(50) NOT NULL, CONSTRAINT pk_contact PRIMARY
-                        KEY(id))
-                        """
-                    )
-                ).execute();
-                new Insert(
-                    session,
-                    new NamedQuery(
-                        "INSERT INTO contact (name) VALUES (:name)",
-                        new TextParam("name", "Joseph Klimber")
-                    )
-                ).execute();
-                new Assertion<>(
-                    "must generated key value",
-                    new ResultSetAsValue<>(
-                        new Select(
-                            session,
-                            new NamedQuery(
-                                "SELECT name FROM contact WHERE id = :id",
-                                new IntParam("id", 1)
-                            )
+        final Source source = new NoAuth(
+            new H2Source(
+                new RandomName().asString()
+            )
+        );
+        try (Session session = source.session()) {
+            new Update(
+                session,
+                new QueryOf(
+                    """
+                    CREATE TABLE contact (id INT AUTO_INCREMENT, name
+                    VARCHAR(50) NOT NULL, CONSTRAINT pk_contact PRIMARY
+                    KEY(id))
+                    """
+                )
+            ).execute();
+            new Insert(
+                session,
+                new NamedQuery(
+                    "INSERT INTO contact (name) VALUES (:name)",
+                    new TextParam("name", "Joseph Klimber")
+                )
+            ).execute();
+            new Assertion<>(
+                "must generated key value",
+                new ResultSetAsValue<>(
+                    new Select(
+                        session,
+                        new NamedQuery(
+                            "SELECT name FROM contact WHERE id = :id",
+                            new IntParam("id", 1)
                         )
-                    ),
-                    new HasValue<>("Joseph Klimber")
-                ).affirm();
-            }
+                    )
+                ),
+                new HasValue<>("Joseph Klimber")
+            ).affirm();
         }
     }
 }
