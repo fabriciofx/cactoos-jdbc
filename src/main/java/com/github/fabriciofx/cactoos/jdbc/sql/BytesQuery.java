e10a20501216335b83d89e571c87cc359037149f
Fabrício Barros Cabral <fabriciofx@gmail.com>
2026-01-18 20:06:31 -0300

    feat: Add BytesQuery that transform a Query in byte[]

---
+++ src/main/java/com/github/fabriciofx/cactoos/jdbc/sql/BytesQuery.java
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (C) 2018-2026 Fabrício Barros Cabral
+ * SPDX-License-Identifier: MIT
+ */
+package com.github.fabriciofx.cactoos.jdbc.sql;
+
+import com.github.fabriciofx.cactoos.jdbc.Params;
+import com.github.fabriciofx.cactoos.jdbc.Query;
+import java.io.ByteArrayOutputStream;
+import org.cactoos.Bytes;
+import org.cactoos.bytes.BytesOf;
+
+/**
+ * BytesQuery.
+ * <p>Convert a {@link Query} into bytes.
+ * @since 0.9.0
+ */
+public final class BytesQuery implements Bytes {
+    /**
+     * Query.
+     */
+    private final Query query;
+
+    /**
+     * Ctor.
+     * @param query The query
+     */
+    public BytesQuery(final Query query) {
+        this.query = query;
+    }
+
+    @Override
+    public byte[] asBytes() throws Exception {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        stream.write(new BytesOf(this.query.sql()).asBytes());
+        for (final Params params : this.query.params()) {
+            stream.write(params.asBytes());
+        }
+        stream.flush();
+        return stream.toByteArray();
+    }
+}
